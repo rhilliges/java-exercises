0512b105af6e3cf0f340a36bfed7f3c3afdce349
Roland Hilliges <roland.hilliges@posteo.net>
2023-05-06 18:06:29 +0000

    median of two arrays

---
+++ src/main/java/com/example/MedianOfTwoArrays.java
@@ -0,0 +1,37 @@
+package com.example;
+
+public class MedianOfTwoArrays {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = 0, n2 = 0;
+        int length = (nums1.length + nums2.length);
+        int[] newArr = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            if (n1 == nums1.length) {
+                newArr[i] = nums2[n2];
+                n2++;
+                continue;
+            }
+            if (n2 == nums2.length) {
+                newArr[i] = nums1[n1];
+                n1++;
+                continue;
+            }
+            if (nums1[n1] < nums2[n2]) {
+                newArr[i] = nums1[n1];
+                n1++;
+            } else {
+                newArr[i] = nums2[n2];
+                n2++;
+            }
+        }
+        boolean uneven = (length % 2) == 1;
+        if(uneven) {
+            return newArr[length/2];
+        } else {
+            return ((double)(newArr[length/2 -1] + newArr[length/2]))/2;
+        }
+    }
+
+}
---
+++ src/test/java/com/example/MedianOfTwoArraysTest.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.TreeMap;
+
+import org.junit.Test;
+
+public class MedianOfTwoArraysTest {
+    @Test
+    public void medianOfTwoArraysTest() {
+        // Input: nums1 = [1,3], nums2 = [2]
+        // Output: 2.00000
+        // Explanation: merged array = [1,2,3] and median is 2.
+        int[] nums1 = new int[] { 1, 3 }, nums2 = new int[] { 2 };
+        MedianOfTwoArrays converter = new MedianOfTwoArrays();
+
+        double median = converter.findMedianSortedArrays(nums1, nums2);
+
+        assertEquals(2, median, 0);
+    }
+
+    @Test
+    public void medianOfTwoArraysTestV2() {
+        int[] nums1 = new int[] { 1, 2 }, nums2 = new int[] { 3,4 };
+        MedianOfTwoArrays converter = new MedianOfTwoArrays();
+
+        double median = converter.findMedianSortedArrays(nums1, nums2);
+
+        assertEquals(2.5, median, .5);
+    }
+}
