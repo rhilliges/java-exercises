949ba171c250bf4e17c368bb64523706b406f0bd
brolyn <roland.hilliges@posteo.net>
2023-05-09 18:57:46 +0000

    test commit

--- src/test/java/com/example/TwoSumTest.java
+++ src/test/java/com/example/TwoSumTest.java
@@ -6,7 +6,7 @@ public class TwoSumTest {
     @Test
     public void testTwoSum() {
         TwoSum converter = new TwoSum();
-        // Output: [[1,2]]
+        // Output: [1,2]
         int[] r = converter.twoSum(new int[]{3,2,4}, 6);
         System.out.println(r);
     }
